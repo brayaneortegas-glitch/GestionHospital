270da87241e3e36cfeb2860508a2d4d5f9a1e814
brayaneortegas-glitch <brayane.ortegas@autonoma.edu.co>
2026-09-14 06:15:00 -0500

    Creación código de clase Persona

--- src/main/java/gestionHospital/Persona.java
+++ src/main/java/gestionHospital/Persona.java
@@ -1,4 +1,33 @@
 package gestionHospital;
 
 public abstract class Persona {
+    private String nombre;
+    private String dni;
+    private int edad;
+    private String genero;
+
+    public Persona(String nombre, String dni, int edad, String genero) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.edad = edad;
+        this.genero = genero;
+    }
+
+    public abstract void presentarse();
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
 }
