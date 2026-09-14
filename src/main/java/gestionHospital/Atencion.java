1ee63bdc46d24e8082064e5982f61ed2713622f5
brayaneortegas-glitch <brayane.ortegas@autonoma.edu.co>
2026-09-14 06:27:50 -0500

    Creación código de clase Atención

--- src/main/java/gestionHospital/Atencion.java
+++ src/main/java/gestionHospital/Atencion.java
@@ -1,4 +1,29 @@
 package gestionHospital;
 
 public class Atencion {
+    public void procesarIngreso(Paciente paciente) {
+
+        String eps = paciente.getEps();
+
+        switch (eps) {
+            case "Sura":
+                System.out.println("Paciente " + paciente.getNombre()
+                        + " asignado al Pabellón A");
+                break;
+
+            case "Sanitas":
+                System.out.println("Paciente " + paciente.getNombre()
+                        + " asignado al Pabellón B");
+                break;
+
+            case "Nueva EPS":
+                System.out.println("Paciente " + paciente.getNombre()
+                        + " asignado al Pabellón C");
+                break;
+
+            default:
+                System.out.println("Paciente " + paciente.getNombre()
+                        + " asignado al Pabellón General");
+        }
+    }
 }
