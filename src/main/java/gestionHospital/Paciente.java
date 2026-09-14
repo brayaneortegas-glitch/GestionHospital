40427d8724220398a97105522ea01cc50b62efcf
brayaneortegas-glitch <brayane.ortegas@autonoma.edu.co>
2026-09-14 06:24:12 -0500

    Creación código de clase Paciente que hereda de Persona

--- src/main/java/gestionHospital/Paciente.java
+++ src/main/java/gestionHospital/Paciente.java
@@ -1,4 +1,60 @@
 package gestionHospital;
 
 public class Paciente extends Persona {
+    
+    private String numeroHistorial;
+    private String eps;
+    private double saldoDisponible;
+    private String sintomas;
+
+    public Paciente(String nombre, String dni, int edad, String genero,
+                    String numeroHistorial, String eps,
+                    double saldoDisponible, String sintomas) {
+
+        super(nombre, dni, edad, genero);
+
+        this.numeroHistorial = numeroHistorial;
+        this.eps = eps;
+        this.saldoDisponible = saldoDisponible;
+        this.sintomas = sintomas;
+    }
+
+    @Override
+    public void presentarse() {
+        System.out.println("Soy el paciente " + getNombre());
+    }
+
+    public void mostrarSintomas() {
+        System.out.println("Síntomas: " + sintomas);
+    }
+
+    public void pagarConsulta() {
+
+        double costoConsulta = 50.0;
+
+        if (saldoDisponible >= costoConsulta) {
+
+            saldoDisponible = saldoDisponible - costoConsulta;
+
+            System.out.println(
+                    "El paciente " + getNombre()
+                            + " intenta pagar $" + costoConsulta
+                            + " Saldo actual $" + (saldoDisponible + costoConsulta)
+                            + " Resultado Éxito"
+            );
+
+        } else {
+
+            System.out.println(
+                    "El paciente " + getNombre()
+                            + " intenta pagar $" + costoConsulta
+                            + " Saldo actual $" + saldoDisponible
+                            + " Resultado Rechazado"
+            );
+        }
+    }
+
+    public String getEps() {
+        return eps;
+    }
 }
