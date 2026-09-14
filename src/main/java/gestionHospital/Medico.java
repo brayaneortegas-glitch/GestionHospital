ac8193f0f6d4b9db1c26b20f9fb649c7c1c18d0a
brayaneortegas-glitch <brayane.ortegas@autonoma.edu.co>
2026-09-14 06:21:05 -0500

    Creación código de clase Medico que hereda de Persona

--- src/main/java/gestionHospital/Medico.java
+++ src/main/java/gestionHospital/Medico.java
@@ -1,4 +1,34 @@
 package gestionHospital;
 
+import java.util.ArrayList;
+
 public class Medico extends Persona {
+    private String especialidad;
+    private String numeroRegistro;
+    private ArrayList<Paciente> pacientes;
+
+    public Medico(String nombre, String dni, int edad, String genero,
+                  String especialidad, String numeroRegistro) {
+
+        super(nombre, dni, edad, genero);
+
+        this.especialidad = especialidad;
+        this.numeroRegistro = numeroRegistro;
+        this.pacientes = new ArrayList<>();
+    }
+
+    @Override
+    public void presentarse() {
+        System.out.println("Soy el Dr. " + getNombre());
+    }
+
+    public void agregarPaciente(Paciente paciente) {
+        pacientes.add(paciente);
+    }
+
+    public void revisarPacientes() {
+        for (Paciente paciente : pacientes) {
+            System.out.println(paciente.getNombre());
+        }
+    }
 }
