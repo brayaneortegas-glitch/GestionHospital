6ee8548baf9132fc55a4a123eccfd450f31e3c58
brayaneortegas-glitch <brayane.ortegas@autonoma.edu.co>
2026-09-14 06:26:36 -0500

    Creación código de clase MedicoCirujano que hereda de Medico e implementa ICirujano

--- src/main/java/gestionHospital/MedicoCirujano.java
+++ src/main/java/gestionHospital/MedicoCirujano.java
@@ -1,4 +1,33 @@
 package gestionHospital;
 
-public class MedicoCirujano extends Medico {
+public class MedicoCirujano extends Medico implements ICirujano {
+    private int numeroQuirofano;
+
+    public MedicoCirujano(String nombre, String dni, int edad, String genero,
+                          String especialidad, String numeroRegistro,
+                          int numeroQuirofano) {
+
+        super(nombre, dni, edad, genero, especialidad, numeroRegistro);
+
+        this.numeroQuirofano = numeroQuirofano;
+    }
+
+    @Override
+    public void presentarse() {
+        System.out.println("Soy el Dr. " + getNombre() + ", cirujano.");
+    }
+
+    @Override
+    public void realizarOperacion(boolean quirofanoDisponible) {
+
+        if (quirofanoDisponible) {
+            System.out.println("El Dr. " + getNombre()
+                    + " realizó la operación en el quirófano "
+                    + numeroQuirofano);
+        } else {
+            System.out.println("El Dr. " + getNombre()
+                    + " no pudo realizar la operación porque el quirófano "
+                    + "no está disponible.");
+        }
+    }
 }
