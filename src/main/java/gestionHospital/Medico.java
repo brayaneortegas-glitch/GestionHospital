be226db4ed9c830dc6b0aa2d8565931d7bfd55f0
brayaneortegas-glitch <brayane.ortegas@autonoma.edu.co>
2026-09-14 06:22:24 -0500

    Creación código de clase Medico que hereda de Persona

--- src/main/java/gestionHospital/Medico.java
+++ src/main/java/gestionHospital/Medico.java
@@ -31,4 +31,33 @@ public class Medico extends Persona {
             System.out.println(paciente.getNombre());
         }
     }
+
+    public int asignarPrioridad(String sintoma) {
+
+        switch (sintoma) {
+            case "Infarto":
+                return 1;
+
+            case "Fiebre":
+                return 2;
+
+            case "Dolor":
+                return 3;
+
+            default:
+                return 0;
+        }
+    }
+
+    public void evaluarSintoma(String sintoma) {
+
+        int prioridad = asignarPrioridad(sintoma);
+
+        System.out.println(
+                "El Dr. " + getNombre()
+                        + " evaluó el síntoma " + sintoma
+                        + " y asignó Prioridad " + prioridad
+        );
+    }
+
 }
